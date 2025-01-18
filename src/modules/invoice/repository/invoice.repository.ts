97f490b66986fa8dfe089b672f98413e3c313fee
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2025-01-18 16:26:55 -0300

    feat(repository): cria invoice

---
+++ src/modules/invoice/repository/invoice.repository.ts
@@ -0,0 +1,64 @@
+import Address from "../../@shared/domain/value-object/address";
+import Id from "../../@shared/domain/value-object/id.value-object";
+import Invoice from "../domain/invoice";
+import InvoiceItem from "../domain/invoice-item";
+import InvoiceGateway from "../gateway/invoice.gateway";
+import { InvoiceItemModel } from "./invoice.item.model";
+import { InvoiceModel } from "./invoice.model";
+
+export class InvoiceRepository implements InvoiceGateway {
+  async find(id: string): Promise<Invoice> {
+    const invoice = await InvoiceModel.findOne({
+      where: { id },
+      include: [InvoiceItemModel],
+    });
+    return new Invoice({
+      id: new Id(invoice.id),
+      name: invoice.name,
+      document: invoice.document,
+      address: new Address({
+        street: invoice.street,
+        number: invoice.number,
+        complement: invoice.complement,
+        city: invoice.city,
+        state: invoice.state,
+        zipCode: invoice.zipCode,
+      }),
+      items: invoice.items.map(
+        (item) =>
+          new InvoiceItem({
+            id: new Id(item.id),
+            name: item.name,
+            price: item.price,
+          })
+      ),
+      createdAt: invoice.createdAt,
+      updatedAt: invoice.updatedAt,
+    });
+  }
+  async save(invoice: Invoice): Promise<void> {
+    await InvoiceModel.create(
+      {
+        id: invoice.id.id,
+        name: invoice.name,
+        document: invoice.document,
+        street: invoice.address.street,
+        number: invoice.address.number,
+        complement: invoice.address.complement,
+        city: invoice.address.city,
+        state: invoice.address.state,
+        zipCode: invoice.address.zipCode,
+        items: invoice.items.map((item: InvoiceItem) => ({
+          id: item.id.id,
+          name: item.name,
+          price: item.price,
+        })),
+        createdAt: invoice.createdAt,
+        updatedAt: invoice.updatedAt,
+      },
+      {
+        include: [InvoiceItemModel],
+      }
+    );
+  }
+}
