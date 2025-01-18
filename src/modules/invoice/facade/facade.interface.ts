27a51fd48315ddec79dabccdcae4d4de9a7a7ff8
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2025-01-18 16:28:04 -0300

    feat(interface): invoice

---
+++ src/modules/invoice/facade/facade.interface.ts
@@ -0,0 +1,63 @@
+export interface GenerateInvoiceInputDto {
+  name: string;
+  document: string;
+  street: string;
+  number: string;
+  complement: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  items: {
+    id?: string;
+    name: string;
+    price: number;
+  }[];
+}
+
+export interface GenerateInvoiceOutputDto {
+  id: string;
+  name: string;
+  document: string;
+  street: string;
+  number: string;
+  complement: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  items: {
+    id: string;
+    name: string;
+    price: number;
+  }[];
+  total: number;
+}
+
+export interface FindInvoiceUseCaseInputDTO {
+  id: string;
+}
+
+export interface FindInvoiceUseCaseOutputDTO {
+  id: string;
+  name: string;
+  document: string;
+  address: {
+    street: string;
+    number: string;
+    complement: string;
+    city: string;
+    state: string;
+    zipCode: string;
+  };
+  items: {
+    id: string;
+    name: string;
+    price: number;
+  }[];
+  total: number;
+  createdAt: Date;
+}
+
+export default interface InvoiceFacadeInterface {
+  generate(input: GenerateInvoiceInputDto): Promise<GenerateInvoiceOutputDto>;
+  find(input: FindInvoiceUseCaseInputDTO): Promise<FindInvoiceUseCaseOutputDTO>;
+}
