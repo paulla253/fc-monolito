459c794a76ac4d57ceacd06ea5e81d3b94309fea
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2025-01-18 16:25:26 -0300

    feat(usecase): cria find invoice

---
+++ src/modules/invoice/usecase/find-invoice/find-invoice.usecase.ts
@@ -0,0 +1,41 @@
+import UseCaseInterface from "../../../@shared/usecase/use-case.interface";
+import InvoiceGateway from "../../gateway/invoice.gateway";
+import {
+  FindInvoiceUseCaseInputDTO,
+  FindInvoiceUseCaseOutputDTO,
+} from "./find-invoice.dto";
+
+export default class FindInvoiceUseCase implements UseCaseInterface {
+  constructor(private invoiceRepository: InvoiceGateway) {}
+
+  async execute(
+    input: FindInvoiceUseCaseInputDTO
+  ): Promise<FindInvoiceUseCaseOutputDTO> {
+    const invoice = await this.invoiceRepository.find(input.id);
+
+    const total = invoice.calculateTotal();
+
+    return {
+      id: invoice.id.id,
+      name: invoice.name,
+      document: invoice.document,
+      address: {
+        street: invoice.address.street,
+        number: invoice.address.number,
+        complement: invoice.address.complement,
+        city: invoice.address.city,
+        state: invoice.address.state,
+        zipCode: invoice.address.zipCode,
+      },
+      items: invoice.items.map((item) => {
+        return {
+          id: item.id.id,
+          name: item.name,
+          price: item.price,
+        };
+      }),
+      total,
+      createdAt: invoice.createdAt,
+    };
+  }
+}
