a3cedb0238b92b52344b7539a1d9ab7874f1babb
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2025-01-20 09:27:09 -0300

    fix: import

--- src/modules/invoice/facade/invoice.facade.spec.ts
+++ src/modules/invoice/facade/invoice.facade.spec.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize-typescript";
 import { InvoiceItemModel } from "../repository/invoice.item.model";
 import { InvoiceModel } from "../repository/invoice.model";
-import InvoiceFacadeFactory from "../factory/payment.facade.factory";
+import InvoiceFacadeFactory from "../factory/invoice.facade.factory";
 
 describe("InvoiceFacade test", () => {
   let sequelize: Sequelize;
